d4b5ddbcaeeb4ec44c242a4bfe8090301606423e
Hans Johnson <hans-johnson@uiowa.edu>
2025-07-01 12:14:55 -0500

    COMP: Add Qt version check for QString conversion in nodeID assignment

--- Base/QTCore/qSlicerCoreIOManager.cxx
+++ Base/QTCore/qSlicerCoreIOManager.cxx
@@ -1046,7 +1046,11 @@ bool qSlicerCoreIOManager::exportNodes(const QList<qSlicerIO::IOProperties>& par
 
     // Copy parameters map; we will need to set the nodeID parameter to correspond to the node in the temporary scene
     qSlicerIO::IOProperties temporarySceneParameters = parameters;
-    temporarySceneParameters["nodeID"] = temporaryStorableNode->GetID();
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 15, 0))
+    temporarySceneParameters["nodeID"] = QString::fromUtf8(temporaryStorableNode->GetID());
+#else
+    temporarySceneParameters["nodeID"] = QString(temporaryStorableNode->GetID());
+#endif
 
     // Deduce "fileType" from "fileFormat" parameter; saveNodes will want both
     qSlicerIO::IOFileType fileType = this->fileWriterFileType(storableNode, parameters["fileFormat"].toString());

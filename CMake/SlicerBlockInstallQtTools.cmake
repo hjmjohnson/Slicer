457ce8e4abfdfaaac11c597e831dd46e87d8d24f
Hans Johnson <hans-johnson@uiowa.edu>
2025-06-30 19:30:23 -0500

    WIP: Disable some advanced features

--- CMake/SlicerBlockInstallQtTools.cmake
+++ CMake/SlicerBlockInstallQtTools.cmake
@@ -34,40 +34,41 @@ foreach(var ${expected_existing_vars})
   endif()
 endforeach()
 
-set(Slicer_INSTALLED_QT_TOOLS)
 
 if(Slicer_BUILD_I18N_SUPPORT)
+  set(Slicer_INSTALLED_QT_TOOLS)
   # Bundle Qt language tools with the application if internationalization is enabled.
   # These tools allow Slicer modules to update and process Qt translation (.ts) files
   # without requiring installation of Qt.
   list(APPEND Slicer_INSTALLED_QT_TOOLS
-    lconvert
-    lrelease
-    lupdate
+    # lconvert removed in Qt6
+    linguist # Qt Linguist GUI
+    lrelease # Compile .ts to .qm
+    lupdate  # extract strings from code
   )
-endif()
 
-foreach(tool IN LISTS Slicer_INSTALLED_QT_TOOLS)
-  set(tool_executable ${qt_root_dir}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX})
-  if(NOT EXISTS "${tool_executable}")
-    message(FATAL_ERROR "Qt tool ${tool} not found: ${tool_executable}")
-  endif()
-  install(PROGRAMS ${tool_executable}
-    DESTINATION ${Slicer_INSTALL_BIN_DIR}
-    COMPONENT Runtime
-    )
-  slicerStripInstalledLibrary(
-    FILES "${Slicer_INSTALL_BIN_DIR}/${tool}"
-    COMPONENT Runtime
-    )
-  if(APPLE)
-    set(dollar "$")
-    install(CODE
-      "set(app ${Slicer_INSTALL_BIN_DIR}/${tool})
-      set(appfilepath \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${dollar}{app}\")
-      message(\"CPack: - Adding rpath to ${dollar}{app}\")
-      execute_process(COMMAND install_name_tool -add_rpath @loader_path/..  ${dollar}{appfilepath})"
+  foreach(tool IN LISTS Slicer_INSTALLED_QT_TOOLS)
+    set(tool_executable ${qt_root_dir}/bin/${tool}${CMAKE_EXECUTABLE_SUFFIX})
+    if(NOT EXISTS "${tool_executable}")
+      message(FATAL_ERROR "Qt tool ${tool} not found: ${tool_executable}")
+    endif()
+    install(PROGRAMS ${tool_executable}
+      DESTINATION ${Slicer_INSTALL_BIN_DIR}
       COMPONENT Runtime
       )
-  endif()
-endforeach()
+    slicerStripInstalledLibrary(
+      FILES "${Slicer_INSTALL_BIN_DIR}/${tool}"
+      COMPONENT Runtime
+      )
+    if(APPLE)
+      set(dollar "$")
+      install(CODE
+        "set(app ${Slicer_INSTALL_BIN_DIR}/${tool})
+        set(appfilepath \"${dollar}ENV{DESTDIR}${dollar}{CMAKE_INSTALL_PREFIX}/${dollar}{app}\")
+        message(\"CPack: - Adding rpath to ${dollar}{app}\")
+        execute_process(COMMAND install_name_tool -add_rpath @loader_path/..  ${dollar}{appfilepath})"
+        COMPONENT Runtime
+        )
+    endif()
+  endforeach()
+endif()

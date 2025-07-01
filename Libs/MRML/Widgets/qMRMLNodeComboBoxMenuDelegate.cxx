8e361c6aca7fbed2e3b3a76ab8de7fdc7688ef2b
Hans Johnson <hans-johnson@uiowa.edu>
2025-07-01 07:25:18 -0500

    COMP: Add QT version check for tabWidth usage in qMRMLNodeComboBoxMenuDelegate
    In Qt 5, QStyleOptionMenuItem had a tabWidth member that controlled the space reserved for a tab character (e.g., for aligning shortcut text). It was often set to 0 to disable tab-based spacing.
    
    In Qt 6, tabWidth was removed as part of a general cleanup of the style options API, since it’s no longer used by the style engine.

--- Libs/MRML/Widgets/qMRMLNodeComboBoxMenuDelegate.cxx
+++ Libs/MRML/Widgets/qMRMLNodeComboBoxMenuDelegate.cxx
@@ -148,7 +148,9 @@ QStyleOptionMenuItem qMRMLNodeComboBoxMenuDelegate::getStyleOption(
     }
     menuOption.text = index.model()->data(index, Qt::DisplayRole).toString()
                            .replace(QLatin1Char('&'), QLatin1String("&&"));
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     menuOption.tabWidth = 0;
+#endif
     menuOption.maxIconWidth =  option.decorationSize.width() + 4;
     menuOption.menuRect = option.rect;
     menuOption.rect = option.rect;
